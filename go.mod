a91e12a53dc66ad4d625033efc35570a1db2ccdf
julian sanchez <julian.sanchez@silstech.com.ar>
2021-08-16 20:12:24 -0300

    First Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dondarrion91/helloPlatziMod
+
+go 1.16
---
+++ utils.go
@@ -0,0 +1,8 @@
+package utils
+
+import "fmt"
+
+// funcion publica
+func HelloWorld() {
+	fmt.Println("Hello World from julian")
+}
